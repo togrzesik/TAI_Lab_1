5e5bcbf8051ebf3612715b69e98b69954afbfec1
togrzesik <tomasz_grzesik.98@outlook.com>
2020-03-17 13:06:19 +0100

    extract navbar code

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let nav = '<nav class="nav">\n' +
+    '<a class="nav-link active" href="index.html">Home</a>\n' +
+    '<a class="nav-link" href="quiz.html">Quiz</a>\n' +
+    '<a class="nav-link" href="contact.html">Kontakt</a>\n' +
+    '</nav>';
+
+const element = document.querySelector(".navbar");
+element.innerHTML = nav;
